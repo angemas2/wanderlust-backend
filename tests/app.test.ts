f8f4b53c0a7a91bcc303a781a2ed0c8154b3b776
Angélique <angelique.massolo@gmail.com>
2022-12-12 12:31:59 +0100

    tests

---
+++ tests/app.test.ts
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const users = require("../routes/users");
+
+/*it("POST /signin", async () => {
+  const res = await request(users).post("/users").send({
+    email: "john@gmail.com",
+    password: "azerty123",
+  });
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body.result).toBe(true);
+});*/
+
+
