a76837444b7e5fc77d26e48a4756082890437a4b
Angélique <angelique.massolo@gmail.com>
2022-12-16 13:14:32 +0100

    add get list of itineraries route

---
+++ build/routes/activities.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var router = express.Router();
+require("../models/connection");
+const Activity = require("../models/activities");
+router.get("/", (req, res) => {
+    Activity.find({ city: req.params.city })
+        .populate("itineraries_id")
+        .then((data) => {
+        if (data) {
+            res.json({ result: true, data: data });
+        }
+        else {
+            res.json({ result: false, error: "no itinerary" });
+        }
+    });
+});
+module.exports = router;
--- routes/itineraries.ts
+++ routes/itineraries.ts
@@ -13,7 +13,7 @@ router.post("/addItinerary", (req: Request, res: Response) => {
   Itinerary.findOne({ name: req.body.name }).then((data: IItinerary) => {
     if (!data) {
       const NewItinerary = new Itinerary({
-        profile_id: req.params.profile_id,
+        profile_id: req.body.profile_id,
         viewpoints_id: viewpointsList,
         km: req.body.km,
         map: req.body.map,
@@ -32,7 +32,7 @@ router.post("/addItinerary", (req: Request, res: Response) => {
       NewItinerary.save().then((x: any) => {
         Itinerary.findById(x._id)
           .populate("viewpoints_id")
-          .then((data: IItinerary) => res.json({ result:true, data }));
+          .then((data: IItinerary) => res.json({ result: true, data }));
       });
     } else {
       res.json({ result: false, error: "itinerary already exists" });
@@ -41,13 +41,15 @@ router.post("/addItinerary", (req: Request, res: Response) => {
 });
 
 router.get("/:city", (req: Request, res: Response) => {
-  Itinerary.find({ profile_id: req.params.city }).then((data: IItinerary) => {
-    if (data) {
-      res.json({ result: true, data: data });
-    } else {
-      res.json({ result: false, error: "no itinerary" });
-    }
-  });
+  Itinerary.find({ city: req.params.city, public: true })
+    .populate("viewpoints_id")
+    .then((data: IItinerary) => {
+      if (data) {
+        res.json({ result: true, data: data });
+      } else {
+        res.json({ result: false, error: "no itinerary" });
+      }
+    });
 });
 
 module.exports = router;
