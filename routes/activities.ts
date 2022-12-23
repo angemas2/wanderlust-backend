fdfc6c20e0b4f97c59ed557c12fb0148bd671634
Lumpa12 <damien.thevenin@gate-16.com>
2022-12-23 09:35:41 +0100

    clean code

--- routes/activities.ts
+++ routes/activities.ts
@@ -1,20 +1,18 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-require("../models/connection");
-import { Request, Response } from "express";
-import { IActivity } from "../models/activities";
+require('../models/connection');
+import { Request, Response } from 'express';
+import { IActivity } from '../models/activities';
 
-const Activity = require("../models/activities");
+const Activity = require('../models/activities');
 
-router.get("/", (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
   Activity.find({ city: req.params.city })
-    .populate("itineraries_id")
+    .populate('itineraries_id')
     .then((data: IActivity) => {
-      if (data) {
-        res.json({ result: true, data: data });
-      } else {
-        res.json({ result: false, error: "no itinerary" });
-      }
+      data
+        ? res.json({ result: true, data: data })
+        : res.json({ result: false, error: 'no itinerary' });
     });
 });
 
--- routes/viewpoints.ts
+++ routes/viewpoints.ts
@@ -1,11 +1,11 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-require("../models/connection");
-import { Request, Response } from "express";
-import { IPoi } from "../models/viewpoints";
-const Viewpoint = require("../models/viewpoints");
+require('../models/connection');
+import { Request, Response } from 'express';
+import { IPoi } from '../models/viewpoints';
+const Viewpoint = require('../models/viewpoints');
 
-router.post("/addPoint", (req: Request, res: Response) => {
+router.post('/addPoint', (req: Request, res: Response) => {
   Viewpoint.findOne({ location: req.body.location }).then((data: IPoi) => {
     if (!data) {
       const NewViewpoint = new Viewpoint({
@@ -22,31 +22,25 @@ router.post("/addPoint", (req: Request, res: Response) => {
     } else {
       res.json({
         result: true,
-        message: "viewpoint already exists",
+        message: 'viewpoint already exists',
         data: data,
       });
     }
   });
 });
 
-router.get("/:name", (req: Request, res: Response) => {
+router.get('/:name', (req: Request, res: Response) => {
   Viewpoint.findOne({ name: req.params.name }).then((data: IPoi) => {
-    if (data === null) {
-      res.json({ result: false, error: "point not found" });
-    } else {
-      res.json({ result: true });
-    }
+    data === null
+      ? res.json({ result: false, error: 'point not found' })
+      : res.json({ result: true });
   });
 });
 
-router.get("/", (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
   Viewpoint.find({ name: req.params.name }).then((data: IPoi) => {
     //check if there is data
-    if (data) {
-      res.json({ result: false, error: "no data" });
-    } else {
-      res.json({ result: data });
-    }
+    data ? res.json({ result: false, error: 'no data' }) : res.json({ result: data });
   });
 });
 
